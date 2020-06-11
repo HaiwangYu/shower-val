6dfff88b22728bf3ec842e7616974278661bc9c7
HaiwangYu <yuhw.pku@gmail.com>
2020-06-10 22:41:16 -0400

    add eval stuff

--- hello_scn.py
+++ hello_scn.py
@@ -6,6 +6,8 @@ import numpy as np
 from model import ResNet
 from model import Hello
 
+from timeit import default_timer as timer
+
 file = uproot.open('data/nue_6350_26_1313.root')
 tblob = file['T_rec_charge_blob']
 
@@ -13,16 +15,23 @@ x = tblob.array('x')
 y = tblob.array('y')
 z = tblob.array('z')
 q = tblob.array('q')
+print(len(x))
+print('x: {} {}'.format(np.min(x), np.max(x)))
+print('y: {} {}'.format(np.min(y), np.max(y)))
+print('z: {} {}'.format(np.min(z), np.max(z)))
+print('q: {} {}'.format(np.min(q), np.max(q)))
 
-# x = np.linspace(0, 4, num=10)
-# y = np.linspace(0, 9, num=10)
-# z = np.linspace(0, 4, num=10)
-# q = np.linspace(42., 42., num=10)
+max_pos = 10000
+x = np.linspace(0, max_pos-1, num=max_pos)
+y = np.linspace(0, max_pos-1, num=max_pos)
+z = np.linspace(0, max_pos-1, num=max_pos)
+q = np.linspace(42., 42., num=max_pos)
 
 pos = np.stack((x,y,z), axis=1)
 q = np.expand_dims(q, axis=1)
 # q = np.stack((q,q,q), axis=1)
 
+# input visualize
 # fig = plt.figure()
 # ax = fig.add_subplot(111, projection='3d')
 # img = ax.scatter(x, y, z, c=q, cmap=plt.jet())
@@ -37,8 +46,26 @@ if use_cuda:
 else:
     print("Not using CUDA.")
 
-model = ResNet()
+model = ResNet(dimension=3, device=device)
 
+# check the SparseTenSor
+input_layer = model.inputLayer
+st = input_layer([torch.LongTensor(pos),torch.FloatTensor(q).to(device)])
+sl = st.get_spatial_locations()
+print('spatial_locations: ', sl.shape)
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# img = ax.scatter(sl[:,0], sl[:,1], sl[:,2], cmap=plt.jet())
+# fig.colorbar(img)
+# plt.show()
+
+nreapeat = 100
+# leave warmup outside the timer
 output = model([torch.LongTensor(pos),torch.FloatTensor(q).to(device)])
+start = timer()
+for rep in range(nreapeat):
+    output = model([torch.LongTensor(pos),torch.FloatTensor(q).to(device)])
+end = timer()
+print('Forward time: {0:.1f} ms'.format((end-start)/nreapeat*1000))
 
-print('Output SparseConvNetTensor:', output.shape)
+print('Output SparseConvNetTensor:', output)
