893c07661a5384491d1bdfd344347c87b3ccf1cc
HaiwangYu <yuhw.pku@gmail.com>
2020-06-23 17:49:57 -0400

    progress bar in training

--- train-deep-vtx.py
+++ train-deep-vtx.py
@@ -9,6 +9,7 @@ from model import Hello
 from model import ResNet
 from model import DeepVtx
 
+import sys
 from timeit import default_timer as timer
 import csv
 import util
@@ -32,35 +33,64 @@ criterion = nn.BCELoss().to(device)
 optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0005)
 
 dir_checkpoint = 'checkpoints/'
-max_samples = 100
-nepoch = 1
+outfile_loss = open(dir_checkpoint+'/loss.txt','w')
+ntrain = 1000
+nval = 200
+nepoch = 50
 start = timer()
 for epoch in range(nepoch):
-    print('epoch: ', epoch)
-    sample = []
-    with open('list1.csv') as f:
+
+    # setup toolbar
+    toolbar_width = 20
+    sys.stdout.write("epoch %d : [%s]" % (epoch, " " * toolbar_width))
+    sys.stdout.flush()
+    sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+    
+    train_loss = 0
+    with open('list1-train.csv') as f:
         reader = csv.reader(f, delimiter=' ')
         isample = 0
         for row in reader:
-            if isample > max_samples :
+            if isample > ntrain :
                 break
             coords, ft = util.load_vtx(row, vis=False)
-            # print('{}, {}'.format(np.min(coords), np.max(coords)))
-            # coords, _ = util.gen_sample()
-            # print('{}, {}'.format(np.min(coords), np.max(coords)))
-            # get truth from ft
             truth = torch.FloatTensor(ft[:,-1]).to(device)
             # remove the truth from ft
             ft = ft[:,0:-1]
             prediction = model([torch.LongTensor(coords),torch.FloatTensor(ft).to(device)])
-            if isample%20 == 0 :
-                print('isample: {}'.format(isample))
+            if isample%(ntrain/toolbar_width) == 0 :
+                sys.stdout.write("=")
+                sys.stdout.flush()
+                # print('\tisample: {}'.format(isample))
                 # util.vis_prediction(coords, prediction)
             loss = criterion(prediction[0:10,0].view(-1),truth[0:10].view(-1))
+            train_loss += loss.item()
             loss.backward()
             optimizer.step()
             isample = isample + 1
     torch.save(model.state_dict(), dir_checkpoint + 'CP{}.pth'.format(epoch))
+    train_loss = train_loss / isample
+    sys.stdout.write("]\n")
+    
+    # validation
+    val_loss = 0
+    with open('list1-val.csv') as f:
+        reader = csv.reader(f, delimiter=' ')
+        isample = 0
+        for row in reader:
+            if isample > nval :
+                break
+            coords, ft = util.load_vtx(row, vis=False)
+            truth = torch.FloatTensor(ft[:,-1]).to(device)
+            # remove the truth from ft
+            ft = ft[:,0:-1]
+            prediction = model([torch.LongTensor(coords),torch.FloatTensor(ft).to(device)])
+            loss = criterion(prediction[0:10,0].view(-1),truth[0:10].view(-1))
+            val_loss += loss.item()
+            isample = isample + 1
+    val_loss = val_loss / isample
+    print('epoch {} finished - tloss: {:.6f} vloss: {:.6f}'.format(epoch, train_loss, val_loss))
+    print('{}, {:.6f}, {:.6f}'.format(epoch, train_loss, val_loss), file=outfile_loss, flush=True)
 end = timer()
 if nepoch > 0:
     print('time/epoch: {0:.1f} ms'.format((end-start)/nepoch*1000))
